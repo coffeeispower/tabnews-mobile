111bf61cb27cb3d91f3b34619fcf0940627e00a1
Tiago Dinis <tiagodinis33@gmail.com>
2022-07-13 16:12:25 +0000

    Add reload feature on tabnews home

--- lib/post_list.dart
+++ lib/post_list.dart
@@ -5,30 +5,52 @@ import 'package:tabnews/get_contents.dart';
 import 'post.dart';
 import 'post_entry_widget.dart';
 
-class PostList extends StatelessWidget {
+class PostList extends StatefulWidget {
   const PostList({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return PostListState();
+  }
+}
+
+class PostListState extends State<PostList> {
+  late List<Post> _posts;
+  late Future<List<Post>> _futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    _futurePosts = fetchPosts();
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: fetchPosts(),
+        future: _futurePosts,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<Post> posts = snapshot.data! as List<Post>;
-            return ListView.separated(
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                var username = posts[index].username;
-                var slug = posts[index].slug;
-                return PostEntry(
-                    key: Key(username + slug + index.toString()),
-                    username: username,
-                    slug: slug,
-                    initialPost: posts[index]);
-              },
-            );
+            _posts = snapshot.data! as List<Post>;
+            return RefreshIndicator(
+                onRefresh: () async {
+                  var newPosts = await fetchPosts();
+                  setState(() {
+                    _posts = newPosts;
+                  });
+                },
+                child: ListView.separated(
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                  itemCount: _posts.length,
+                  itemBuilder: (context, index) {
+                    var username = _posts[index].username;
+                    var slug = _posts[index].slug;
+                    return PostEntry(
+                        key: Key(username + slug + index.toString()),
+                        username: username,
+                        slug: slug,
+                        initialPost: _posts[index]);
+                  },
+                ));
           } else if (snapshot.hasError) {
             return const Center(
               child: Text('Poxa! Não foi possivel carregar os posts!'),
