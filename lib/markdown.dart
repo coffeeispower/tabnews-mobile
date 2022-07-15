8d84fb13c2df5eacdf6f96acb8c55925bc503c30
Tiago Dinis <tiagodinis33@gmail.com>
2022-07-15 13:48:03 +0000

    Add syntax highlighting on code blocks

--- lib/markdown.dart
+++ lib/markdown.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_highlighter/themes/atom-one-light.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:markdown/markdown.dart' as md;
+import 'package:flutter_highlighter/flutter_highlighter.dart';
+import 'package:flutter_highlighter/themes/atom-one-dark.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Markdown extends StatelessWidget {
   const Markdown(this.body) : super(key: null);
@@ -19,6 +23,9 @@ class Markdown extends StatelessWidget {
           .replaceAll(RegExp("\\<h6\\>"), "###### ")
           .replaceAll(RegExp("\\<\\/*.\\>"), ""),
       inlineSyntaxes: [md.InlineHtmlSyntax()],
+      builders: {
+        'code': CodeElementBuilder(),
+      },
       extensionSet: md.ExtensionSet(
         md.ExtensionSet.gitHubFlavored.blockSyntaxes,
         [md.EmojiSyntax(), ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes],
@@ -41,6 +48,44 @@ class Markdown extends StatelessWidget {
               : Colors.grey[900],
           borderRadius: const BorderRadius.all(Radius.circular(10)),
         ),
+      ),
+    );
+  }
+}
+
+class CodeElementBuilder extends MarkdownElementBuilder {
+  @override
+  Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    var language = '';
+
+    if (element.attributes['class'] != null) {
+      String lg = element.attributes['class'] as String;
+      language = lg.substring(9);
+    }
+    return SizedBox(
+      width:
+          MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.width,
+      child: HighlightView(
+        // The original code to be highlighted
+        element.textContent,
+
+        // Specify language
+        // It is recommended to give it a value for performance
+        language: language,
+
+        // Specify highlight theme
+        // All available themes are listed in `themes` folder
+        theme: MediaQueryData.fromWindow(WidgetsBinding.instance.window)
+                    .platformBrightness ==
+                Brightness.light
+            ? atomOneLightTheme
+            : atomOneDarkTheme,
+
+        // Specify padding
+        padding: const EdgeInsets.all(8),
+
+        // Specify text style
+        textStyle: GoogleFonts.jetBrainsMono(),
       ),
     );
   }
